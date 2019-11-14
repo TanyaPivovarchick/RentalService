fe807dbaa02acdb1e51f7e42cf743e43187f4b11
Tatyana Pivovarchick <tatsiana.pivovarchick@gmail.com>
2019-11-14 14:47:27 +0300

    Add base repository

---
+++ RentalService.DAL/Contracts/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RentalService.DAL.Contracts
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int? id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ RentalService.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalService.DAL.Contracts;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RentalService.DAL.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly RentalServiceContext db;
+
+        public BaseRepository(RentalServiceContext context)
+        {
+            db = context;
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await db.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<T> GetAsync(int? id)
+        {
+            return await db.Set<T>().FindAsync(id);
+        }
+
+        public virtual void Add(T entity)
+        {
+             db.Set<T>().Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            db.Set<T>().Update(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            db.Set<T>().Remove(entity);
+        }
+    }
+}
