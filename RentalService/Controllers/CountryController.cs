b697905d63f9d3b1ffe191aac0b5b1047b9859d6
Tatyana Pivovarchick <tatsiana.pivovarchick@gmail.com>
2019-11-14 15:05:15 +0300

    Add controller and views for Country

---
+++ RentalService/Controllers/CountryController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentalService.BL.Contracts;
+using RentalService.BL.DTO;
+using RentalService.ViewModels;
+
+namespace RentalService.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly ICountryService service;
+
+        public CountryController(ICountryService countryService)
+        {
+            service = countryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var countries = await service.GetAllCountriesAsync();
+
+            return View(countries?.Adapt<IEnumerable<CountryVM>>());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest("Country ID wasn't set");
+            }
+
+            try
+            {
+                var country = await service.GetCountryAsync(id);
+
+                return View(country.Adapt<CountryVM>());
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Name, Code")] CountryVM country)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.AddCountry(country.Adapt<CountryDTO>());
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(country);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest("Country ID wasn't set");
+            }
+
+            try
+            {
+                var country = await service.GetCountryAsync(id);
+
+                return View(country.Adapt<CountryVM>());
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit([Bind("Id, Name, Code")] CountryVM country)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await service.UpdateCountry(country.Adapt<CountryDTO>());
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CountryExists(country.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(country);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest("Country ID wasn't set");
+            }
+
+            try
+            {
+                var country = await service.GetCountryAsync(id);
+
+                return View(country.Adapt<CountryVM>());
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await service.DeleteCountry(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountryExists(int id)
+        {
+            return service.CountryExists(id);
+        }
+    }
+}
--- RentalService/ViewModels/CountryVM.cs
+++ RentalService/ViewModels/CountryVM.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace RentalService.BL.DTO
+namespace RentalService.ViewModels
 {
-    public class CountryDTO
+    public class CountryVM
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Code { get; set; }
     }
 }
