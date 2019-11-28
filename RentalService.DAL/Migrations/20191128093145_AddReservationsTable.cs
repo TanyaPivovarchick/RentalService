388a9aaf0665a4c0111278ed3c6b5c92096b069d
Tatyana Pivovarchick <tatsiana.pivovarchick@gmail.com>
2019-11-28 12:32:29 +0300

    Add reservations table

---
+++ RentalService.DAL/Migrations/20191128093145_AddReservationsTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentalService.DAL.Migrations
+{
+    public partial class AddReservationsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsConfirmed = table.Column<bool>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    KeyReceiptTime = table.Column<TimeSpan>(nullable: false),
+                    Cost = table.Column<double>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    RentalPointCarId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_RentalPointCars_RentalPointCarId",
+                        column: x => x.RentalPointCarId,
+                        principalTable: "RentalPointCars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_RentalPointCarId",
+                table: "Reservations",
+                column: "RentalPointCarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserId",
+                table: "Reservations",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+        }
+    }
+}
